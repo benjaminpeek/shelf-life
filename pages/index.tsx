51a36e2f7da50ed380f3de312e570f372b7cfb10
Benjamin Peek <benjaminpeek8@gmail.com>
2025-06-06 16:21:14 -0600

    testing making the dashboard now

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-	return <h1 className="text-4xl text-purple-600">baba works!</h1>;
+	return (
+		<>
+			<h1 className="text-4xl text-purple-600">baba works!</h1>
+			<p>dude</p>
+		</>
+	);
 }
